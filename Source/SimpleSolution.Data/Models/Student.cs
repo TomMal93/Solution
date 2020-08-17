1898928ed43a3ad662711e8dee08450ed71ed9a2
Tomasz <Tomasz.Maleszyk93@gmail.com>
2020-08-17 18:29:46 +0200

    EF - model validation

--- Source/SimpleSolution.Data/Models/Student.cs
+++ Source/SimpleSolution.Data/Models/Student.cs
@@ -1,12 +1,17 @@
 ﻿using SimpleSolution.Data.ModelsBase;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SimpleSolution.Data.Models
 {
     public class Student : EntityBase
     {
         public Guid StudentId { get; set; }
+        [Required(ErrorMessage = "Proszę wprowadzić imię.")]
+        [StringLength(50)]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Proszę wprowadzić nazwisko.")]
+        [StringLength(50)]
         public string Surname { get; set; }
 
         public Guid ClassroomId { get; set; }
