c8fa5bf32d0b1a0a4924c762a26ec3f6738f0d5d
Tomasz <Tomasz.Maleszyk93@gmail.com>
2020-08-17 19:17:47 +0200

    EF - Initial Migration

---
+++ Source/SimpleSolution.Data/Migrations/202008171700230_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿namespace SimpleSolution.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Classrooms",
+                c => new
+                    {
+                        ClassroomId = c.Guid(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        CreateDate = c.DateTime(),
+                        ModifyDate = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.ClassroomId);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        StudentId = c.Guid(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Surname = c.String(nullable: false, maxLength: 50),
+                        ClassroomId = c.Guid(nullable: false),
+                        CreateDate = c.DateTime(),
+                        ModifyDate = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.StudentId)
+                .ForeignKey("dbo.Classrooms", t => t.ClassroomId, cascadeDelete: true)
+                .Index(t => t.ClassroomId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Students", "ClassroomId", "dbo.Classrooms");
+            DropIndex("dbo.Students", new[] { "ClassroomId" });
+            DropTable("dbo.Students");
+            DropTable("dbo.Classrooms");
+        }
+    }
+}
