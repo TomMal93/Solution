e306efda503feb1fc64cff89ae5969d58dcbdc0a
Tomasz <Tomasz.Maleszyk93@gmail.com>
2020-08-17 18:43:18 +0200

    EF - DbContext

---
+++ Source/SimpleSolution.Data/DAL/SimpleContext.cs
@@ -0,0 +1,30 @@
+﻿using SimpleSolution.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSolution.Data.DAL
+{
+    public class SimpleContext : DbContext
+    {
+        public SimpleContext() : base("SimpleSolutionContext")
+        {
+
+        }
+
+        public DbSet<Classroom> Classrooms { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+        }
+
+    }
+}
