27463ccf51593a185964b69c26693c79c267e5ff
Tomasz <Tomasz.Maleszyk93@gmail.com>
2020-08-17 22:00:59 +0200

    Add Home viewModel

---
+++ Source/SimpleSolution/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SimpleSolution.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleSolution.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Student> Students { get; set; }
+        public IEnumerable<Classroom> Classrooms { get; set; }
+    }
+}
