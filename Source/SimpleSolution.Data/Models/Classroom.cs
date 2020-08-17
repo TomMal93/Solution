e39dd6640227456cf7b7afc729b0c4c8ed8166bf
Tomasz <Tomasz.Maleszyk93@gmail.com>
2020-08-17 18:27:41 +0200

    EF - Models

---
+++ Source/SimpleSolution.Data/Models/Classroom.cs
@@ -0,0 +1,14 @@
+﻿using SimpleSolution.Data.ModelsBase;
+using System;
+using System.Collections.Generic;
+
+namespace SimpleSolution.Data.Models
+{
+    public class Classroom : EntityBase
+    {
+        public Guid ClassroomId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
---
+++ Source/SimpleSolution.Data/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using SimpleSolution.Data.ModelsBase;
+using System;
+
+namespace SimpleSolution.Data.Models
+{
+    public class Student : EntityBase
+    {
+        public Guid StudentId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Guid ClassroomId { get; set; }
+
+        public virtual Classroom Classroom { get; set; }
+    }
+}
